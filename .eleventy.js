b7b53d682934669fa4867866901ef31ff5ea719e
abiddiscombe <76795128+abiddiscombe@users.noreply.github.com>
2024-09-27 22:34:08 +0100

    chore: Add robots.txt file.

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,9 @@ module.exports = function (config) {
         'src/_assets/font': './font',
         'src/_assets/media': './media',
 
+        // Passthrough the Robots TXT.
+        'src/_assets/robots.txt': './robots.txt',
+
         // Passthrough the PostCSS output.
         'src/_assets/main.out.css': './main.css',
     });
