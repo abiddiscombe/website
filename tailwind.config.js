931bda3b01fc8c348a7d406dbac3dfcd512fc8c6
abiddiscombe <76795128+abiddiscombe@users.noreply.github.com>
2024-10-23 15:16:27 +0100

    feat: Remove JetBrains font.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+import colors from "tailwindcss/colors";
+
 module.exports = {
   content: ["./src/**/*.{njk,md}", "./src/**/*.svg"],
   theme: {
     extend: {
+      colors: {
+        primary: colors.zinc,
+      },
       fontFamily: {
-        sans: ["JetBrains Mono", "Menlo", "Consolas", "Monaco", "Liberation Mono", "Lucida Console", "monospace"],
+        sans: ["-apple-system", "BlinkMacSystemFont", "avenir next", "avenir", "segoe ui", "helvetica neue", "helvetica", "Cantarell", "Ubuntu", "roboto", "noto", "arial", "sans-serif"],
       },
     }
   },
