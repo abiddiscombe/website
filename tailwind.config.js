a1c60ad12b3b31ba9bcac8dc4276f4a9ceb12a8c
abiddiscombe <76795128+abiddiscombe@users.noreply.github.com>
2024-12-10 18:29:52 +0000

    feat: (4) UI update.

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
       },
       fontFamily: {
         sans: ["-apple-system", "BlinkMacSystemFont", "avenir next", "avenir", "segoe ui", "helvetica neue", "helvetica", "Cantarell", "Ubuntu", "roboto", "noto", "arial", "sans-serif"],
+        mono: ["Menlo", "Consolas", "Monaco", "Liberation Mono", "Lucida Console", "monospace"]
       },
     }
   },
